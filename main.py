0219b586fe3f69e7798ce2940afbb9d660fe5928
Yegor Trepalin <yegortrep-ipt27@lll.kpi.ua>
2024-01-09 20:34:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from keras.models import Model
 from transformers import BertTokenizer, TFBertModel
 
 # set cuda gpu
-print(tf.config.list_physical_devices('GPU'))
+print(tf.config.list_physical_devices("GPU"))
 # physical_devices = tf.config.list_physical_devices('GPU')
 #
 # if len(physical_devices) > 0:
@@ -21,14 +21,18 @@ x_train = [" ".join([str(word) for word in sequence]) for sequence in x_train]
 x_test = [" ".join([str(word) for word in sequence]) for sequence in x_test]
 
 # Initialize the BERT tokenizer
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
 # Tokenize the input sequences
-x_train_tokens = tokenizer(x_train, padding=True, truncation=True, return_tensors="tf", max_length=512)
-x_test_tokens = tokenizer(x_test, padding=True, truncation=True, return_tensors="tf", max_length=512)
+x_train_tokens = tokenizer(
+    x_train, padding=True, truncation=True, return_tensors="tf", max_length=512
+)
+x_test_tokens = tokenizer(
+    x_test, padding=True, truncation=True, return_tensors="tf", max_length=512
+)
 
 # Load the pre-trained BERT model
-bert_model = TFBertModel.from_pretrained('bert-base-uncased')
+bert_model = TFBertModel.from_pretrained("bert-base-uncased")
 
 # Freeze BERT layers
 for layer in bert_model.layers:
@@ -38,16 +42,18 @@ for layer in bert_model.layers:
 inputs = Input(shape=(512,), dtype=tf.int32)
 bert_output = bert_model(inputs)["last_hidden_state"]
 pooled_output = bert_output[:, 0, :]
-outputs = Dense(1, activation='sigmoid')(pooled_output)
+outputs = Dense(1, activation="sigmoid")(pooled_output)
 
 # Create the model
 model = Model(inputs=inputs, outputs=outputs)
 
 # Compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
 # Train the model
-model.fit(x_train_tokens["input_ids"], y_train, epochs=1, batch_size=32, validation_split=0.2)
+model.fit(
+    x_train_tokens["input_ids"], y_train, epochs=1, batch_size=32, validation_split=0.2
+)
 
 # Evaluate the model on the test set
 accuracy = model.evaluate(x_test_tokens["input_ids"], y_test)[1]
